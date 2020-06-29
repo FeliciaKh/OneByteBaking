4e027a8a192d5731e543c576470c3094111d9423
FeliciaKh <feliciakh0428@gmail.com>
2020-06-28 20:40:58 -0400

    Update CSS

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardTitle, IonCardHeader } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+// import ExploreContainer from '../components/ExploreContainer';
 import './Browse.css';
 // import {Recipe} from './Recipe'
-import recipeData from './data'
+// import recipeData from './data'
 
 
 const Browse: React.FC = () => {
@@ -11,23 +11,30 @@ const Browse: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Browse</IonTitle>
+          <IonTitle>Browse all our recipes!</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
+      <IonContent class='card-background-page'>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Browse</IonTitle>
           </IonToolbar>
         </IonHeader>
+
         <IonCard>
           <IonCardHeader>
             <img src='https://www.recipetineats.com/wp-content/uploads/2016/08/Brownies_0.jpg' />
-            <IonCardTitle>Brownies</IonCardTitle> 
+            <IonCardTitle class= 'light'>Brownies</IonCardTitle> 
           </IonCardHeader>
         </IonCard>
-        <ExploreContainer name="Browser page"> 
-        </ExploreContainer>
+
+        <IonCard>
+          <IonCardHeader>
+            <img src='https://www.chelseasmessyapron.com/wp-content/uploads/2019/02/Bakery-Style-Chocolate-Chip-Cookies-1.jpg' />
+            <IonCardTitle>Chocolate Chip Cookies</IonCardTitle> 
+          </IonCardHeader>
+        </IonCard>
+        
       </IonContent>
     </IonPage>
   );
