0ff7ca415c520b148474c1778f391195ad6685e2
FeliciaKh <feliciakh0428@gmail.com>
2020-06-28 20:54:33 -0400

    Update recipe card data

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -34,6 +34,62 @@ const Browse: React.FC = () => {
             <IonCardTitle>Chocolate Chip Cookies</IonCardTitle> 
           </IonCardHeader>
         </IonCard>
+
+        <IonCard>
+          <IonCardHeader>
+            <img src='https://themodernproper.sfo2.cdn.digitaloceanspaces.com/posts/2018/_recipeSquare/easy-tiramisu-11.jpg?mtime=20181021075447&focal=none&tmtime=20200211015326' />
+            <IonCardTitle>Tiramisu</IonCardTitle> 
+          </IonCardHeader>
+        </IonCard>
+
+        <IonCard>
+          <IonCardHeader>
+            <img src= 'https://www.bigbearswife.com/wp-content/uploads/2017/01/Caramel-Banana-Pudding-Parfaits-7.jpg' />
+            <IonCardTitle>Caramel Banana Pudding</IonCardTitle>
+          </IonCardHeader>
+        </IonCard>
+
+        <IonCard>
+          <IonCardHeader>
+            <img src= 'https://www.biggerbolderbaking.com/wp-content/uploads/2015/04/BBB69-Churros-Chocolate-Thumbnail-v.1.jpg' />
+            <IonCardTitle class='light'>Churros</IonCardTitle>
+          </IonCardHeader>
+        </IonCard>
+
+        <IonCard>
+          <IonCardHeader>
+            <img src= 'https://www.restlesschipotle.com/wp-content/uploads/2015/06/deep-fried-oreos-feat.jpg' />
+            <IonCardTitle>Deep Fried Oreos</IonCardTitle>
+          </IonCardHeader>
+        </IonCard>
+
+        <IonCard>
+          <IonCardHeader>
+            <img src= 'https://cookiesandcups.com/wp-content/uploads/2017/10/bestsnickerdoodles-266x266.jpg' />
+            <IonCardTitle class='light'>Snickerdoodle Cookies</IonCardTitle>
+          </IonCardHeader>
+        </IonCard>
+
+        <IonCard>
+          <IonCardHeader>
+            <img src= 'https://cdn.shopify.com/s/files/1/0163/5948/9636/articles/unicorn-cake-nerdy-nummeis_1024x1024.jpg?v=1547142809' />
+            <IonCardTitle>Unicorn Cake</IonCardTitle>
+          </IonCardHeader>
+        </IonCard>
+
+        <IonCard>
+          <IonCardHeader>
+            <img src= 'https://www.homecookingadventure.com/images/recipes/Chocolate_Mirror_Cake_main.jpg' />
+            <IonCardTitle class='light'>Chocolate Mirror Glaze Cake</IonCardTitle>
+          </IonCardHeader>
+        </IonCard>
+
+        <IonCard>
+          <IonCardHeader>
+            <img src= 'https://images-gmi-pmc.edge-generalmills.com/b6a2a4e7-73f5-4aec-9bb6-f2b5054d65e6.jpg' />
+            <IonCardTitle class='light'>Dutch Apple Pie</IonCardTitle>
+          </IonCardHeader>
+        </IonCard>
         
       </IonContent>
     </IonPage>
